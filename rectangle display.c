8b967386edfc54b041e0687ae667fcbff12cdcbb
vishnu-idk <vishnurajeev1812@gmail.com>
2026-04-05 23:47:31 +0530

    rectangle display.c

---
+++ rectangle display.c
@@ -0,0 +1,12 @@
+//to display length and breadth of a rectangle in floating point datatype
+#include <stdio.h>
+void main()
+{
+    float a,b;
+    printf("write the length of a rectangle");
+    scanf("%f",&a);
+    printf("write the breadth of the rectangle");
+    scanf("%f",&b);
+    
+    printf("the length of the rectangle is %f\nthe breadth of the rectangle is %f",a,b);
+}
